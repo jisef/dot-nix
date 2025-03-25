e56eaab38201ee4f1859daf8f9d45e6486fec300
jisef <josef@sigma.com>
2025-03-25 09:32:18 +0100

    added git config

--- config/git.nix
+++ config/git.nix
@@ -1,3 +1,9 @@
 {config, pkgs, inputs, ... }: { 
-
- }
+  home-manager.users.josef = {
+    programs.git = {
+      enable = true;
+      userName = "josef";
+      userEmail = "josef@sigma.kys";
+    };
+  };
+}
