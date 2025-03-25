17c28788fa8e60845939fab38169efc51e3ca131
jisef <josef@sigma.com>
2025-03-25 21:55:12 +0100

    added simple waybar config

--- config/bar.nix
+++ config/bar.nix
@@ -2,6 +2,22 @@
   home-manager.users.josef = {
     programs.waybar = {
       enable = true;
+      settings = {
+        mainBar = {
+          layer = "top";
+          height = 30;
+          modules-right = ["battery" "clock"];
+          "clock" = {
+            format = "{:%H:%M}";
+            format-alt = "{:%Y-%m-%d}";
+          };
+
+          "battery" = {
+            interval = 60;
+
+          };
+        };
+      };
     };
   };
 }
