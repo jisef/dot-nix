ba19bc6772a7fff88fde6995df800e4bd4f231f1
jisef <josef@sigma.com>
2025-03-27 19:48:48 +0100

    added basic firefox config

--- config/firefox.nix
+++ config/firefox.nix
@@ -1,23 +1,48 @@
-{config, pkgs, inputs, ...}: {
-  programs.firefox = {
-    enable = true;
-    languagePacks = [ "en-GB" "de"];
-    profiles.default = {
-      settings = {
-        "extensions.activeThemeID" = "firefox-compact-dark@mozilla.org";
-        "browser.theme.content-theme" = 0;
-        "browser.theme.toolbar-theme" = 0;
+{ config, pkgs, inputs, ... }: {
+  environment.systemPackages = with pkgs; [ firefox ];
+  home-manager.users.josef = {
+    programs.firefox = {
+      enable = true;
+      languagePacks = [ "en-GB" "de" ];
+      profiles.default = {
+        bookmarks = {
+          force = true;
+          settings = [
+            {
+              name = "cloud";
+              # tags = ["sigma"];
+              keyword = "ex";
+              url = "https://www.icloud.com/";
+            }
+          ];  
+        };
+        extensions = with inputs.firefox-addons; [
+          
+        ];
+
+        settings = {
+          "extensions.activeThemeID" = "firefox-compact-dark@mozilla.org";
+          "browser.theme.content-theme" = 0;
+          "browser.theme.toolbar-theme" = 0;
+          "browser.newtabpage.activity-stream.showSponsored" = false;
+          "browser.newtabpage.activity-stream.system.showSponsored" = false;
+          "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
+          "browser.newtabpage.activity-stream.feeds.section.topstories" = false;
+          "browser.newtabpage.activity-stream.feeds.topsites" = false;
+          "browser.bookmarks.addedImportButton" = false;
+
+          "app.normandy.first_run" = false;
+          "app.update.auto" = false;
+
+          "browser.bookmarks.restore_default_bookmarks" = false;
+          "browser.contentblocking.category" = "standard";        #change !!!
+        };
+      };
+      policies = {
+        BlockAboutConfig = true;
+        DefaultDownloadDirectory = "/home/josef/Downloads";
+        DisableTelemetry = true;
       };
     };
-    policies = {
-      BlockAboutConfig = true;
-      DefaultDownloadDirectory = "/home/josef/Downloads";
-      DisableTelemetry = false;
-      ExtensionSettings = {
-        "uBlock0@raymondhill.net" = {
-        installation_mode = "force_installed";
-        install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
-      };
-    };
-  };  
-};
+  };
+}
