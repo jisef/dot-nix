d336616fa490445ff144c7ed0b290249dfa29ac5
josef <josef@sigma.kys>
2025-05-07 10:46:54 +0200

    renamed shell to terminal

---
+++ config/terminal.nix
@@ -0,0 +1,84 @@
+{ config, pkgs, inputs, ... }: {
+
+  environment.systemPackages = with pkgs; [ zsh oh-my-zsh zoxide direnv];
+
+  programs.zsh.enable = true;
+
+  home-manager.users.josef = {
+    programs.zoxide = {
+      enable = true;
+      enableBashIntegration = true;
+      enableZshIntegration = true;
+    };
+
+    programs.zsh = {
+      enable = true;
+      enableCompletion = true;
+
+      shellAliases = {
+        c = "clear";
+        clearfetch = "clear && fastfetch";
+        ls = "eza";
+        cd = "z";
+      };
+
+      oh-my-zsh = {
+        plugins = [ "git" "zsh-autosuggestions" "zsh-autocomplete" ];
+      };
+    };
+
+    programs.starship = {
+      enable = true;
+      enableZshIntegration = true;
+      settings = {
+        right_format = "$cmd_duration";
+        add_newline = false;
+
+        format = ''
+          $directory$hostname$git_branch$git_status
+          $character'';
+
+        character = {
+          success_symbol = "[➜](bold green)";
+          error_symbol = "[✗](bold red)";
+        };
+
+        cmd_duration = {
+          min_time =
+            2; # Show duration for commands taking longer than 2 seconds
+          format = "[⏱ $duration]($style)";
+          style = "bold green";
+        };
+
+        directory = {
+          use_logical_path = true;
+          truncation_length = 6;
+          home_symbol = "~";
+          truncate_to_repo = false;
+          style = "bold cyan";
+        };
+
+        git_branch = {
+          symbol = " ";
+          style = "bold purple";
+          format = "[$symbol$branch]($style) ";
+        };
+
+        git_status = {
+          ahead = "⇡";
+          behind = "⇣";
+          diverged = "⇕";
+          modified = "✎";
+          deleted = "✖";
+          style = "bold yellow";
+        };
+
+        hostname = {
+          ssh_only = true;
+          format = "[on $hostname](bold yellow)";
+          disabled = false;
+        };
+      };
+    };
+  };
+}
